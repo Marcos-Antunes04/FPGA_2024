12a0e09cda40424c8f3c78c739d442194d8d67ea
fnazario <francisconazario0136@gmail.com>
2024-04-16 11:53:49 -0300

    comentarios

--- Projeto1/AULA_1.vhd
+++ Projeto1/AULA_1.vhd
@@ -4,10 +4,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity AULA_1 is
     Port ( DIPSW : in  STD_LOGIC_VECTOR (3 downto 0);
-           BUT : in  STD_LOGIC_VECTOR (3 downto 0);
+           BUT : in  STD_LOGIC_VECTOR (3 downto 0); --input never read
            LEDS: out  STD_LOGIC_VECTOR (3 downto 0);
-           CLK27MHZ : in  STD_LOGIC;
-           GPIO : inout  STD_LOGIC_VECTOR (7 downto 0));
+           CLK27MHZ : in  STD_LOGIC; --input never read
+           GPIO : inout  STD_LOGIC_VECTOR (7 downto 0)); --input never read
 end AULA_1;
 
 architecture Comportamento of AULA_1 is
