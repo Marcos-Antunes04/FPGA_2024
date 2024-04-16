7ab1758e4aaf3900737274afae5a60419c0c3c37
fnazario <francisconazario0136@gmail.com>
2024-04-16 12:11:37 -0300

    test commit

--- Projeto1/AULA_1.vhd
+++ Projeto1/AULA_1.vhd
@@ -3,7 +3,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity AULA_1 is
-    Port ( DIPSW : in  STD_LOGIC_VECTOR (3 downto 0);
+    Port ( DIPSW : in  STD_LOGIC_VECTOR (3 downto 0); --dsldasl
            BUT : in  STD_LOGIC_VECTOR (3 downto 0); --input never read
            LEDS: out  STD_LOGIC_VECTOR (3 downto 0);
            CLK27MHZ : in  STD_LOGIC; --input never read
