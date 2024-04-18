23846c39e78eae5eaefef46e7704eb3f3c5bb756
Marcos-Antunes04 <mca4729@gmail.com>
2024-04-18 02:49:22 +0000

    Updated mux.vhd and constraints.ucf

--- Projeto1/mux.vhd
+++ Projeto1/mux.vhd
@@ -10,9 +10,25 @@ end mux;
 architecture Behavioral of mux is
 	
 begin
-	Y(0) <= not(Mux(0)) and not(Mux(1));
-	Y(1) <= Mux(0) and not(Mux(1));
-	Y(2) <= not(Mux(0)) and Mux(1);
-	Y(3) <= Mux(0) and Mux(1);
+	--Y(0) <= (not(Mux(0))) and (not(Mux(1)));
+	--Y(1) <= Mux(0) and (not(Mux(1)));
+	--Y(2) <= (not(Mux(0))) and Mux(1);
+	--Y(3) <= Mux(0) and Mux(1);
+	
+	multiplexer: process(Mux)
+	begin
+		case Mux is
+			when "00" =>
+				Y <= "0001";
+			when "01" =>
+				Y <= "0010";
+			when "10" =>
+				Y <= "0100";
+			when "11" =>
+				Y <= "1000";
+			when others =>
+				Y <= "1111";
+		end case;
+	end process;
 	
 end Behavioral;
